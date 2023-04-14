f08b8dc9ee01300b0df4a66e12b23ba81a1abcd7
maliukalina <l.malyuk@gmail.com>
2023-04-14 18:50:05 -0400

    Login

--- src/App.js
+++ src/App.js
@@ -5,54 +5,56 @@ import {
   Route,
   NavLink,
 } from "react-router-dom";
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import AddLesson from "./pages/AddLesson";
 import Lesson from "./pages/Lesson";
-import LessonDashboard from "./pages/LessonDashboard";
+import TeacherDashboard from "./pages/TeacherDashboard";
 import LessonStatistics from "./pages/LessonStatistics";
 import Login from "./pages/Login";
 
 export const UserContext = createContext(null);
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    // <UserContext.Provider value={{ user, setUser, jwt, setJwt }}>
-    <Router>
-      <header>
-        <nav
-          style={{
-            padding: ".3em",
-            display: "flex",
-            justifyContent: "space-between",
-          }}
-        >
-          <NavLink exact to="/Login">
-            Login
-          </NavLink>
-          <NavLink exact to="/LessonDashboard">
-            Lesson Dashboard
-          </NavLink>
-          <NavLink exact to="/AddLesson">
-            Add Lesson
-          </NavLink>
-          <NavLink exact to="/Lesson">
-            Lesson
-          </NavLink>
-          <NavLink exact to="/LessonStatistics">
-            Lesson Statistics
-          </NavLink>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/LessonStatistics" element={<LessonStatistics />} />
-        <Route path="/AddLesson" element={<AddLesson />} />
-        <Route path="/Lesson" element={<Lesson />} />
-        <Route path="/LessonDashboard" element={<LessonDashboard />} />
-        {/* <Route path ='/Login' element={<Login/>} />*/}
-        <Route exact path="/Login" element={<Login />} />
-      </Routes>
-    </Router>
-    // </UserContext.Provider>
+    <UserContext.Provider value={{ user, setUser }}>
+      <Router>
+        <header>
+          <nav
+            style={{
+              padding: ".3em",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <NavLink exact to="/Login">
+              Login
+            </NavLink>
+            <NavLink exact to="/TeacherDashboard">
+              Teacher Dashboard
+            </NavLink>
+            <NavLink exact to="/AddLesson">
+              Add Lesson
+            </NavLink>
+            <NavLink exact to="/Lesson">
+              Lesson
+            </NavLink>
+            <NavLink exact to="/LessonStatistics">
+              Lesson Statistics
+            </NavLink>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/LessonStatistics" element={<LessonStatistics />} />
+          <Route path="/AddLesson" element={<AddLesson />} />
+          <Route path="/Lesson" element={<Lesson />} />
+          <Route path="/TeacherDashboard" element={<TeacherDashboard />} />
+          {/* <Route path ='/Login' element={<Login/>} />*/}
+          <Route exact path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,44 +1,55 @@
-import React from "react";
+import React, { useContext } from "react";
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import FormControl from "@mui/material/FormControl";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { UserContext } from "../App";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "t",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDQLs0SdLx1FzA6yCruDoQBs1pzi9N0YAQ",
+  authDomain: "emo-detective-be.firebaseapp.com",
+  projectId: "emo-detective-be",
+  storageBucket: "emo-detective-be.appspot.com",
+  messagingSenderId: "600385549714",
+  appId: "1:600385549714:web:9eef38621b28cea505fd4b",
 };
-
-// const roles = [
-//   {
-//     value: "Student",
-//     label: "Student",
-//   },
-//   {
-//     value: "Teacher",
-//     label: "Teacher",
-//   },
-// ];
 
 export default function Login() {
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
-  // const [name, setName] = useState("");
-  // const [password, setPassword] = useState("");
+  const { setUser } = useContext(UserContext);
   // const [role, setRole] = useState("");
+
+  const loginWithGoogle = (event) => {
+    event.preventDefault();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((response) => {
+        let userObj = {
+          role: "teacher",
+          userId: response.user.uid,
+        };
+        console.log(userObj);
+        fetch(`https://emo-detective-be.ue.r.appspot.com/users`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userObj),
+        })
+          .then((apiResponse) => apiResponse.json())
+          .then((data) => {
+            response.user.getIdToken();
+            setUser(data);
+          })
+          .catch(alert);
+      })
+      .catch((err) => alert(err.message));
+  };
 
   return (
     <>
@@ -62,7 +73,14 @@ export default function Login() {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Login with Google
+                Student Login
+              </Button>
+              <Button
+                onClick={loginWithGoogle}
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Teacher Login
               </Button>
             </FormControl>
           </Box>
