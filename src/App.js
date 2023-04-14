a3099a9b06bdad33e57005219eda52dcd44437fa
Ann Starovoitov <annstarovoitov@gmail.com>
2023-04-14 13:33:03 -0400

    added loginform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import LogInForm from './LoginForm';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,8 +17,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+
         </a>
-      </header>
+      
+      </header> */}
+      <LogInForm />
     </div>
   );
 }
