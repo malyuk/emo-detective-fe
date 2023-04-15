a999a5f42b868753c8a52465499b12c4fef29035
sergey <sergey@teaminua.com>
2023-04-14 20:04:20 -0400

    poll stats every 5 s

--- src/api/lessonApi.js
+++ src/api/lessonApi.js
@@ -5,3 +5,26 @@ export const fetchLesson = async (lessonId) => {
   const data = await response.json();
   return data;
 };
+
+export const postStat = async (userId, emotions, engagementScore) => {
+  try {
+    const response = await fetch(
+      `${process.env.REACT_APP_API_BASE_URL}/stats`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: userId,
+          emotions: emotions,
+          engagementScore: engagementScore,
+        }),
+      }
+    );
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
