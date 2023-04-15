ee7e92d5da63eaac42ba3150bb9a817bc3e14c62
Ann Starovoitov <annstarovoitov@gmail.com>
2023-04-14 20:58:26 -0400

    added buttons

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,10 +1,48 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function Home() {
   return (
-    <main>
-      <h1>Welcome</h1>
-    </main>
+    <ThemeProvider theme={theme}>
+      <main>
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        ></Box>
+        <Container maxWidth="sm">
+          <Typography
+            component="h2"
+            variant="h3"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >Lesson
+          </Typography>
+
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+          >
+            <Button variant="contained">Start Lesson</Button>
+            <Button variant="outlined">Stop Lesson</Button>
+          </Stack>
+
+        </Container>
+
+      </main >
+    </ThemeProvider >
   );
 }
 
