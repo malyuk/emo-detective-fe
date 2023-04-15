02baac395ca080fcae9e1c96a5becc050216d489
Ann Starovoitov <annstarovoitov@gmail.com>
2023-04-15 15:40:26 -0400

    chart updated

--- src/BarChart.js
+++ src/BarChart.js
@@ -9,12 +9,12 @@ import { Bar, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'rec
 
 // ]
 const data = [
-  { Time: "2017", angry: 20, disgust: 20,fear: 39, happy: 28, neutral: 90, sad: 8, surprise: 4},
-  { Time: "2018", angry: 30, disgust: 60,fear: 39, happy: 48, neutral: 20, sad: 8, surprise: 4},
-  { Time: "2019", angry: 0, disgust: 20,fear: 39, happy: 69, neutral: 30, sad: 8, surprise: 4},
-  { Time: "2020", angry: 50, disgust: 80,fear: 39, happy: 48, neutral: 60, sad: 8, surprise: 4},
-  { Time: "2021", angry: 70, disgust: 30,fear: 39, happy: 88, neutral: 80, sad: 8, surprise: 4},
-  { Time: "2022", angry: 90, disgust: 50,fear: 39, happy: 48, neutral: 50, sad: 8, surprise: 4},
+  { Time: "2017", angry: 20, disgust: 20, fear: 39, happy: 28, neutral: 90, sad: 8, surprise: 4 },
+  { Time: "2018", angry: 30, disgust: 60, fear: 39, happy: 48, neutral: 20, sad: 8, surprise: 4 },
+  { Time: "2019", angry: 0, disgust: 20, fear: 39, happy: 69, neutral: 30, sad: 8, surprise: 4 },
+  { Time: "2020", angry: 50, disgust: 80, fear: 39, happy: 48, neutral: 60, sad: 8, surprise: 4 },
+  { Time: "2021", angry: 70, disgust: 30, fear: 39, happy: 88, neutral: 80, sad: 8, surprise: 4 },
+  { Time: "2022", angry: 90, disgust: 50, fear: 39, happy: 48, neutral: 50, sad: 8, surprise: 4 },
 ];
 
 export default function ChartBar() {
@@ -27,13 +27,13 @@ export default function ChartBar() {
       <YAxis />
       <Tooltip />
       <Legend />
-      <Bar dataKey="angry" fill="#8884d8" />
-      <Bar dataKey="disgust" fill="#82ca9d" />
-      <Bar dataKey="fear" fill="#82ca9d" />
-      <Bar dataKey="happy" fill="#82ca9d" />
-      <Bar dataKey="neutral" fill="#82ca9d" />
-      <Bar dataKey="sad" fill="#82ca9d" />
-      <Bar dataKey="surprise" fill="#82ca9d" />
+      <Bar dataKey="angry" fill="#f90008" />
+      <Bar dataKey="disgust" fill="#798589" />
+      <Bar dataKey="fear" fill="#0021fc" />
+      <Bar dataKey="happy" fill="#69ff0c" />
+      <Bar dataKey="neutral" fill="#fcc40c" />
+      <Bar dataKey="sad" fill="#754d38" />
+      <Bar dataKey="surprise" fill="#A45EE5" />
     </BarChart>
   )
 }
