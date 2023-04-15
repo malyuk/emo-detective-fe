b203d14b50925f5955f5987e7cae1469b998eb02
maliukalina <l.malyuk@gmail.com>
2023-04-15 11:19:49 -0400

    data from API

--- src/LineChart.js
+++ src/LineChart.js
@@ -1,27 +1,52 @@
-import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import React, { useEffect, useState } from "react";
+import {
+  Line,
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const data = [
-  { name: "2017", react: 32, angular: 37, vue: 60 },
-  { name: "2018", react: 42, angular: 42, vue: 43 },
-  { name: "2019", react: 52, angular: 41, vue: 53 },
-  { name: "2020", react: 60, angular: 41, vue: 27 },
-  { name: "2021", react: 51, angular: 33, vue: 30 },
-  { name: "2022", react: 96, angular: 33, vue: 49 },
+  { Time: "2017", react: 42, angular: 42, vue: 43 },
+  { Time: "2018", react: 42, angular: 42, vue: 43 },
+  { Time: "2019", react: 52, angular: 41, vue: 53 },
+  { Time: "2020", react: 60, angular: 41, vue: 27 },
+  { Time: "2021", react: 51, angular: 33, vue: 30 },
+  { Time: "2022", react: 96, angular: 33, vue: 49 },
 ];
 
-export default function Chart() {
+export default function Chart(lesson) {
+  const [chart, setChart] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://emo-detective-be.ue.r.appspot.com/stats/lesson/etNqwBwosOWg8nFKnr0g`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setChart(data);
+        console.log(data, "here");
+      })
+      .catch(alert);
+  }, [lesson]);
 
   return (
     <LineChart width={600} height={300} data={data}>
       <Line type="monotone" dataKey="react" stroke="#2196F3" strokeWidth={5} />
-      <Line type="monotone" dataKey="angular" stroke="#F44335" strokeWidth={5} />
+      <Line
+        type="monotone"
+        dataKey="angular"
+        stroke="#F44335"
+        strokeWidth={5}
+      />
       <Line type="monotone" dataKey="vue" stroke="#FFCA29" strokeWidth={5} />
       <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="name" />
+      <XAxis dataKey="Time" />
       <YAxis />
       <Tooltip />
       <Legend />
     </LineChart>
-  )
+  );
 }
-
